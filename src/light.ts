73487453670589c641ad4a76b5b66a628fadee76
Kjetil Midtgarden Golid <kjetil.golid@gmail.com>
2022-10-26 16:25:53 +0200

    remove redundant function

--- src/light.ts
+++ src/light.ts
@@ -1,17 +1,7 @@
-import {
-  angleBetweenPointAndCell,
-  angleBetweenPointAndShape,
-  distanceFromPointToCell,
-} from './geometry';
-import { Cell, Shape, Vec } from './interfaces';
+import { angleBetweenPointAndShape } from './geometry';
+import { Shape, Vec } from './interfaces';
 
 const BRIGHTNESS = 1;
-
-export function illuminanceOfCell(sun: Vec, cell: Cell): number {
-  const distance = distanceFromPointToCell(sun, cell);
-  const angle = angleBetweenPointAndCell(sun, cell);
-  return (BRIGHTNESS * Math.abs(Math.cos(angle))) / 200; //(distance * distance);
-}
 
 export function illuminanceOfShape(sun: Vec, shape: Shape): number {
   const angle = angleBetweenPointAndShape(sun, shape);
